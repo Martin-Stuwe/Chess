cacdad0c6b3ce83c2163e185d978152e3f31f324
Martin Stuwe <martinstuwe@googlemail.com>
2020-04-15 19:26:10 +0200

    Added Console.java

---
+++ src/main/java/schach/Console.java
@@ -0,0 +1,15 @@
+package schach;
+import java.util.Scanner;
+
+
+public class Console {
+	
+	Scanner input;
+		
+		public void open() {
+		    Scanner userInput = new Scanner(System.in);
+		    this.input = userInput;
+		}
+		
+
+}
