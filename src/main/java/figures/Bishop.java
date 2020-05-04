146ec4f994918eef67bb679109764f6c41c91d7e
TankR <TankR@DESKTOP-ITCR3ER.localdomain>
2020-05-04 17:04:02 +0200

    adde getBoardVisual

--- src/main/java/figures/Bishop.java
+++ src/main/java/figures/Bishop.java
@@ -3,9 +3,31 @@ package figures;
 import game.Figures;
 
 public class Bishop extends Figures {
-
-	public Bishop() {
-		
-		
+	 private String boardVisual;
+	 private int pos1;
+	 private int pos2;
+	 private String color;
+	 
+	public Bishop(int pos1, int pos2, String color) {
+		this.pos1 = pos1;
+		this.pos2 = pos2;
+		this.color = color;
 	}
+	
+	public String getBoardVisual() {
+		if(this.color =="w") {
+			this.boardVisual = "B";
+			return boardVisual;
+		}
+		else if(this.color =="b") {
+			this.boardVisual = "b";
+			return boardVisual;
+		}
+		else {
+			return boardVisual;
+		}
+			
+	}
+	
 }
+
