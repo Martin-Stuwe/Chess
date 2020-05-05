838da4bfa1397102adb8f3ee7b9ad0d7ee750701
Martin Stuwe <martinstuwe@googlemail.com>
2020-05-05 23:58:45 +0200

    Move method got a built in converter.

--- src/main/java/game/Figures.java
+++ src/main/java/game/Figures.java
@@ -38,68 +38,17 @@ public class Figures {
 	
 	
 	public void move(Board board, char pos1from, int pos2from, char pos1to, int pos2to) { //Reactions in jeder Figurenunterklasse
-		switch (pos1from) {
-		case 'a':
-			pos1from =0;
-			break;
-		case 'b':
-			pos1from =1;
-			break;
-		case 'c':
-			pos1from =2;
-			break;
-		case 'd':
-			pos1from =3;
-			break;
-		case 'e':
-			pos1from =4;
-			break;
-		case 'f':
-			pos1from =5;
-			break;
-		case 'g':
-			pos1from =6;
-			break;
-		case 'h':
-			pos1from =7;
-			break;
-		default:
-			throw new IllegalArgumentException("Invalid input");
-		}
-		switch (pos1to) {
-		case 'a':
-			pos1to =0;
-			break;
-		case 'b':
-			pos1to =1;
-			break;
-		case 'c':
-			pos1to =2;
-			break;
-		case 'd':
-			pos1to =3;
-			break;
-		case 'e':
-			pos1to =4;
-			break;
-		case 'f':
-			pos1to =5;
-			break;
-		case 'g':
-			pos1to =6;
-			break;
-		case 'h':
-			pos1to =7;
-			break;
-		default:
-			throw new IllegalArgumentException("Invalid input");
-		}
-	ValueRange range = java.time.temporal.ValueRange.of(0, 7);
-	if (!range.isValidIntValue(pos2from)&&!range.isValidIntValue(pos2to)) {
-		throw new IllegalArgumentException("Invalid integer range");
-	}
-	pos2from = -1*(pos2from-8);
-	pos2to = -1*(pos2to-8);
+	
+	String ConvertedFrom=board.ConvertMoveInput(board, pos1from, pos2from);	
+	String ConvertedTo = board.ConvertMoveInput(board, pos1to, pos2to);
+	
+	pos1from = ConvertedFrom.charAt(0);
+	pos2from = ConvertedFrom.charAt(1);
+	
+	pos1to = ConvertedTo.charAt(0);   
+	pos2to = ConvertedTo.charAt(1);
+	
+	
 	board.Positionen[pos1to][pos2to] = board.Positionen[pos1from][pos2from];
 	board.Positionen[pos1from][pos2from] = null;
 	
