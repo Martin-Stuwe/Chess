a1f0c9bfebfddddc4af2eb98a5b24c064a91508b
TankR <TankR@DESKTOP-ITCR3ER.localdomain>
2020-05-05 14:46:44 +0200

    updated boardTest

--- src/test/java/game/BoardTest.java
+++ src/test/java/game/BoardTest.java
@@ -1,4 +1,5 @@
 package game;
+import figures.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,8 +8,9 @@ public class BoardTest {
 	@Test
 	public void testGetFieldSetField() {
 		Board board = new Board();
-		board.setField(1, 1, "R");
-		assertEquals("R", board.getField(1, 1), "getField und setField funktionieren");
+		Rook R = new Rook(1,1,"b");
+		board.setField(1, 1, R);
+		assertEquals(R, board.getField(1, 1), "getField und setField funktionieren");
 	}
 	
 }
