13066d618adea798adbe543e0c497340b24cd34c
Fin <fin.tiedemann@student.uni-luebeck.de>
2020-05-11 13:21:42 +0200

    player javadoc + comments

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,30 +1,47 @@
-/**
-* Class for the Chess Players
-* @author Martin Stuwe 676421
-* @author Zeyi Sun
-* @author Richard Tank
-* @author Fin Niklas Tiedemann
-* Gruppe 23
-*/
-
-
 package game;
 
+/**
+ * Class for the Chess Players
+ * @author Martin Stuwe 676421
+ * @author Zeyi Sun
+ * @author Richard Tank
+ * @author Fin Niklas Tiedemann
+ * group 23
+ */
 public class Player {
 	
+	/**
+	 * name of the player
+	 */
 	String name;
+	
+	/**
+	 * color of the player
+	 */
 	String color;
 	
-	//Constructor for Players
+	/**
+	 * constructor for the player
+	 * @param name name of the player as a String
+	 * @param color color of the player as a String
+	 */
 	public Player(String name, String color) { 
 		this.name= name;
 		this.color=color;
 	}
 	
+	/**
+	 * get-method of the color
+	 * @return color 
+	 */
 	public String getColor() {
 		return color;
 	}
 	
+	/**
+	 * get-method of the name
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
