16266d0f2f64fd5611c29a5b1bcf5f387c3777e4
Fin <fin.tiedemann@student.uni-luebeck.de>
2020-05-11 12:20:48 +0200

    zug javadoc

--- src/main/java/game/Zug.java
+++ src/main/java/game/Zug.java
@@ -1,64 +1,94 @@
-/**
-* Class for the moves
-* @author Martin Stuwe 676421
-* @author Zeyi Sun
-* @author Richard Tank
-* @author Fin Niklas Tiedemann
-* Gruppe 23
-*/
-
-
 package game;
 
+/**
+ * Class for the moves
+ * @author Martin Stuwe 676421
+ * @author Zeyi Sun
+ * @author Richard Tank
+ * @author Fin Niklas Tiedemann
+ * group 23
+ */
 public class Zug {
 	
 	/**
-	* The figure that has been moved
-	*/
-	Figures figur;
+	 * the figure that has been moved
+	 */
+	Figures figure;
 	
 	/**
-	* From where the figure has moved
-	*/
+	 * from where the figure has moved x axis position
+	 */
 	int from1;
+	
+	/**
+	 * from where the figure has moved y axis position
+	 */
 	int from2;
 	
 	/**
-	* Where the figure has moved to
-	*/
+	 * where the figure has moved to x axis position
+	 */
 	int to1;
+	
+	/**
+	 * where the figure has moved to y axis position
+	 */
 	int to2;
 	
-	public Zug(Figures figur, int from1, int from2, int to1, int to2) {  //Zug Konstruktor
-		this.figur = figur;
-		this.from1=from1;
-		this.from2=from2;
-		
-		this.to1=to1;
-		this.to2=to2;
-		
+	/**
+	 * constructor for a zug object
+	 * @param figure figure that has been moved
+	 * @param from1 x axis position
+	 * @param from2 y axis position
+	 * @param to1 x axis position
+	 * @param to2 y axis position
+	 */
+	public Zug(Figures figure, int from1, int from2, int to1, int to2) {  
+		this.figure = figure;
+		this.from1 = from1;
+		this.from2 = from2;
+		this.to1 = to1;
+		this.to2 = to2;
 	}
 	
+	/**
+	 * get-method for the figure object
+	 * @return figure figure of the zug object
+	 */
 	public Figures getFigure() {
-		return this.figur;
+		return this.figure;
 	}
 	
+	/**
+	 * get-method for the integer from1
+	 * @return from1 from1 integer of the zug object
+	 */
 	public int getFrom1() {
 		return this.from1;
 	}
 	
+	/**
+	 * get-method for the integer from2
+	 * @return from2 from2 integer of the zug object
+	 */
 	public int getFrom2() {
 		return this.from2;
 	}
 	
+	/**
+	 * get-method for the integer to1
+	 * @return to1 to1 integer of the zug object
+	 */
 	public int getTo1() {
 		return this.to1;
 	}
 	
+	/**
+	 * get-method for the integer to2
+	 * @return to2 to2 integer of the zug object
+	 */
 	public int getTo2() {
 		return this.to2;
 	}
-	
-	
-//Hier die Spielzüge mit Position etc und erlaubten Zügen?
+
 }
