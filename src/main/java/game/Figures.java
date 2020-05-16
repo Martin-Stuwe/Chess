1e2de9e0781d56e998dded00b58ba59e5e6eed0e
Martin Stuwe <martinstuwe@googlemail.com>
2020-05-17 01:06:12 +0200

    reduced complexity

--- src/main/java/game/Figures.java
+++ src/main/java/game/Figures.java
@@ -40,6 +40,10 @@ public class Figures {
 	*/
 	String unallowed = "!Move not allowed";
 	
+	Figures restoreFrom;
+	
+	Figures restoreTo;
+	
 	/**
 	* check if the figure has moved yet
 	*/
@@ -133,9 +137,9 @@ public class Figures {
 	public Boolean move(Board board, int pos1from, int pos2from, int pos1to, int pos2to) { 
 		
 		// saves starting position of figure
-		Figures RestoreFrom=board.positionen[pos1from][pos2from];
+		restoreFrom=board.positionen[pos1from][pos2from];
 		// saves ending position of figure
-		Figures RestoreTo=board.positionen[pos1to][pos2to];
+		restoreTo=board.positionen[pos1to][pos2to];
 		
 		// check if starting position is empty
 		if (board.positionen[pos1from][pos2from] == null) {
@@ -156,9 +160,9 @@ public class Figures {
 		}
 		
 		// check if figure is allowed to make certain move
-		return move2(board, pos1from, pos2from, pos1to,pos2to, RestoreFrom, RestoreTo);
+		return move2(board, pos1from, pos2from, pos1to,pos2to);
 	}
-		public boolean move2(Board board, int pos1from, int pos2from, int pos1to, int pos2to,Figures RestoreFrom, Figures RestoreTo) {
+		public boolean move2(Board board, int pos1from, int pos2from, int pos1to, int pos2to) {
 		if (!board.positionen[pos1from][pos2from].validMove(board,pos1to, pos2to)) {
 			System.out.println(unallowed);
 			return false;
@@ -170,9 +174,9 @@ public class Figures {
 			return false;
 		}
 		
-		return move22(board, pos1from, pos2from, pos1to,pos2to, RestoreFrom, RestoreTo);
+		return move22(board, pos1from, pos2from, pos1to,pos2to);
 		}
-		public boolean move22(Board board, int pos1from, int pos2from, int pos1to, int pos2to,Figures RestoreFrom, Figures RestoreTo) {
+		public boolean move22(Board board, int pos1from, int pos2from, int pos1to, int pos2to) {
 		
 			// color change and adding beaten black figures to the beaten list
 		if(board.getCurrentTurn()==0) {
@@ -189,9 +193,9 @@ public class Figures {
 				}
 			board.setCurrentTurn(0);
 		}
-		return move3(board,pos1from, pos2from,pos1to,pos2to, RestoreFrom, RestoreTo);
+		return move3(board,pos1from, pos2from,pos1to,pos2to);
 		}
-		public boolean move3(Board board,int pos1from, int pos2from, int pos1to, int pos2to, Figures RestoreFrom, Figures RestoreTo) {
+		public boolean move3(Board board,int pos1from, int pos2from, int pos1to, int pos2to) {
 		
 		// moving the figure
 		board.positionen[pos1to][pos2to] = board.getField(pos1from,pos2from);
@@ -202,9 +206,9 @@ public class Figures {
 		// changing the figures position integers
 		board.positionen[pos1to][pos2to].setPos(pos1to, pos2to);
 		
-		return move4(board,pos1from, pos2from, pos1to, pos2to, RestoreFrom, RestoreTo);
+		return move4(board,pos1from, pos2from, pos1to, pos2to);
 		}
-		public boolean move4(Board board, int pos1from, int pos2from, int pos1to, int pos2to, Figures RestoreFrom, Figures RestoreTo) {
+		public boolean move4(Board board, int pos1from, int pos2from, int pos1to, int pos2to) {
 	
 		// check if you are in check after the move		
 		if(board.checkCheck()) {
@@ -212,8 +216,8 @@ public class Figures {
 			// check if black is in check after the move and restoring the move if so
 			if(board.blackCheck){
 				if(board.getCurrentTurn()==0) {
-					 board.positionen[pos1to][pos2to] = RestoreTo;
-					 board.positionen[pos1from][pos2from] = RestoreFrom;
+					 board.positionen[pos1to][pos2to] = restoreTo;
+					 board.positionen[pos1from][pos2from] = restoreFrom;
 					 if(board.positionen[pos1to][pos2to]!= null) {
 						 board.positionen[pos1to][pos2to].setPos(pos1to, pos2to);
 					 }
@@ -227,25 +231,31 @@ public class Figures {
 				 }
 			}
 			
-			// check if white is in check after the move and restoring the move if so
-			else if(board.whiteCheck&&board.getCurrentTurn()==1) {
-						board.positionen[pos1to][pos2to] = RestoreTo;
-						board.positionen[pos1from][pos2from] = RestoreFrom;
-						if(board.positionen[pos1to][pos2to]!= null) {
-							board.positionen[pos1to][pos2to].setPos(pos1to, pos2to);
-						}
-						if(board.positionen[pos1from][pos2from]!= null) {
-							board.positionen[pos1from][pos2from].setPos(pos1from, pos2from);
-						}
-						board.setCurrentTurn(0);
-					
-					System.out.println(unallowed);
-					return false;
-					}
+			else check4(board, pos1to, pos2to, pos1from, pos2from);
 		}	 
 		
 		board.positionen[pos1to][pos2to].setHasMoved(true);
 		return true;
+	}
+		
+		
+	public boolean check4(Board board, int pos1to, int pos2to, int pos1from, int pos2from) {
+		// check if white is in check after the move and restoring the move if so
+		if(board.whiteCheck&&board.getCurrentTurn()==1) {
+					board.positionen[pos1to][pos2to] = restoreTo;
+					board.positionen[pos1from][pos2from] = restoreFrom;
+					if(board.positionen[pos1to][pos2to]!= null) {
+						board.positionen[pos1to][pos2to].setPos(pos1to, pos2to);
+					}
+					if(board.positionen[pos1from][pos2from]!= null) {
+						board.positionen[pos1from][pos2from].setPos(pos1from, pos2from);
+					}
+					board.setCurrentTurn(0);
+				
+				System.out.println(unallowed);
+				return false;
+				}
+		return false;
 	}
 	
 	/**
