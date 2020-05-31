00788cb9af6b5243c58bdd83f69b0e445b3c51e8
TankR <TankR@DESKTOP-ITCR3ER.localdomain>
2020-05-31 09:27:08 +0200

    fixed Figures GUI

--- src/main/java/schach/GuiMain.java
+++ src/main/java/schach/GuiMain.java
@@ -28,6 +28,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
