debf016e6665ab3df76ff50b94771147e9db6fbc
TankR <TankR@DESKTOP-ITCR3ER.localdomain>
2020-06-07 16:07:49 +0200

    added launch argument

--- src/main/java/schach/Main.java
+++ src/main/java/schach/Main.java
@@ -18,8 +18,21 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		  System.out.println("Argument count: " + args.length);
+		    for (int i = 0; i < args.length; i++) {
+		        System.out.println("Argument " + i + ": " + args[i]);
+		    }
+		    if(args.length>0 && args[0].intern()=="--no-gui") {
         System.out.println("starting chess match");
         StartGame.StartGameCommand();
-        
+		    }
+		    else if(args.length>0 && args[0].intern()=="--simple") {
+		        System.out.println("starting chess match");
+		        StartGame.PlayerVsPlayer();
+				    }
+		    else {
+		    	
+		    	GuiMain.main(args); 
+		    }
     }
 }
