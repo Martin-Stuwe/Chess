538cde70ac7b77bfde669c2bb7a3360de802d89e
Fin T. <fin.tiedemann@student.uni-luebeck.de>
2020-07-04 14:52:34 +0200

    Zeitklasse

---
+++ src/main/java/game/Time.java
@@ -0,0 +1,28 @@
+package game;
+
+public class Time {
+	
+	public int timeWhite;
+	
+	public int timeBlack;
+	
+	public boolean active;
+	
+	
+	public Time(int white, int black) {
+		this.timeWhite = white;
+		this.timeBlack = white;
+		this.active = false;
+	}
+	
+	public void updateTime(Board brett) {
+		if((this.timeWhite > 0 || this.timeBlack > 0)&& this.active) {
+			if(brett.getCurrentTurn() == 0) {
+				timeWhite = timeWhite - 1;
+			}
+			if(brett.getCurrentTurn() == 1) {
+				timeBlack = timeBlack - 1;
+			}
+		}
+	}
+}
