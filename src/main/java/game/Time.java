197fece963d8993627f31053a535be4284c2e865
Fin T. <fin.tiedemann@student.uni-luebeck.de>
2020-07-05 12:49:29 +0200

    pmd javadoc Time

--- src/main/java/game/Time.java
+++ src/main/java/game/Time.java
@@ -1,20 +1,46 @@
 package game;
 
+/**
+ * Class for a time object
+ * @author Martin Stuwe 676421
+ * @author Zeyi Sun
+ * @author Richard Tank
+ * @author Fin Niklas Tiedemann
+ * group 23
+ * it3
+ */
 public class Time {
 	
+	/**
+	 * time of the white player as an integer
+	 */
 	public int timeWhite;
 	
+	/**
+	 * time of the black player as an integer
+	 */
 	public int timeBlack;
 	
+	/**
+	 * check if time object is active
+	 */
 	public boolean active;
 	
-	
+	/**
+	 * constructor of a time object
+	 * @param white white's time in seconds
+	 * @param black black's time in seconds
+	 */
 	public Time(int white, int black) {
 		this.timeWhite = white;
-		this.timeBlack = white;
+		this.timeBlack = black;
 		this.active = false;
 	}
 	
+	/**
+	 * method to update the time
+	 * @param brett board the game is on
+	 */
 	public void updateTime(Board brett) {
 		if((this.timeWhite > 0 || timeBlack > 0)&& this.active) {
 			if(brett.getCurrentTurn() == 0) {
