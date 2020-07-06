94957f0738e67667b7d4282df8af3070d56f50ba
Martin Stuwe <martinstuwe@googlemail.com>
2020-07-06 06:29:05 +0200

    pmd fix for unused import

--- src/test/java/schach/ConsoleTest.java
+++ src/test/java/schach/ConsoleTest.java
@@ -1,7 +1,6 @@
 package schach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
